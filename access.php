1885041ca4072914a59b13589e251c7f1e699b97
yacine <lentente1958@gmail.com>
2023-06-20 23:17:23 +0100

    handle cors credentials and origin

--- access.php
+++ access.php
@@ -1,7 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin',"no-cors");
 header('Referrer Policy','no-referrer-when-downgrade');
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin:http://localhost:3000');
+header('Access-Control-Allow-Credentials:true');
 header('Access-Control-Allow-Methods: GET, POST'); 
 header("Access-Control-Allow-Headers: X-Requested-With");
 ?>
