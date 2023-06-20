faf8afea632e8a82874041a006f008b1a80d0bdd
yacine <lentente1958@gmail.com>
2023-06-20 20:43:54 +0100

    checkUser session change implementation

--- session.php
+++ session.php
@@ -6,10 +6,18 @@ function generateId(){
 }
 
 function setSession(){
-    $_SESSION['userId']=generateId();
+    $_SESSION['userId']=[generateId(),time()];
 }
 function checkUser(){
-   return   isset($_SESSION["userId"]) ? true : false ;
+    if(isset($_SESSION["userId"])) {
+            if(time()-$_SESSION['userId']<35){
+                return [true];
+            }else{
+                return [false,"you are ovveride the quiz time "];
+            }
+    } else {
+        return [false,"you are not tracked"];
+     };
 }
 function deleteUser(){
       session_destroy();
